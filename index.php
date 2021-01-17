5bad541afd538ce2ff9534aa7bfeaaba145d103d
Votikk <wojciech.progr@gmail.com>
2021-01-17 18:01:47 +0100

    Poprawka w dodaniu bootstrapa

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="pl">
-
+  
 <head>
   <meta charset="UTF-8">
   <title>Książka adresowa</title>
@@ -10,15 +10,24 @@
   <script src="https://unpkg.com/babel-standalone/babel.js"></script>
   <!-- Bootstrap + JQuery -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js" integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
   <!-- Custom -->
   <script src="custom.js"></script>
   <link rel="stylesheet" src="custom.css">
 </head>
 
 <body>
-  
+  <div class="container">
+    <div class="row">
+      <?php for ($i = 0; $i < 10; $i++) : ?>
+        <div class="col-md-3">
+          <?= $i; ?>
+        </div>
+      <?php endfor; ?>
+    </div>
+  </div>
 </body>
 
 </html>
