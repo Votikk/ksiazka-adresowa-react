09bd1e8c5b43adf45de4cfa18d98ba3a4e014aeb
Votikk <wojciech.progr@gmail.com>
2021-01-17 17:53:52 +0100

    Dodanie JQuery i Bootstrap

--- index.php
+++ index.php
@@ -3,16 +3,22 @@
 
 <head>
   <meta charset="UTF-8">
-  <title>Pierwszy komponent w React.js</title>
+  <title>Książka adresowa</title>
+  <!-- React -->
   <script src="https://unpkg.com/react/umd/react.development.js"></script>
   <script src="https://unpkg.com/react-dom/umd/react-dom.development.js"></script>
   <script src="https://unpkg.com/babel-standalone/babel.js"></script>
+  <!-- Bootstrap + JQuery -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <!-- Custom -->
   <script src="custom.js"></script>
-  <link rel="stylesheet" href="custom.css">
+  <link rel="stylesheet" src="custom.css">
 </head>
 
 <body>
-  Treść strony
+  
 </body>
 
 </html>
