588a22e09e1fbac372476c1654accd6cd41f8709
Votikk <wojciech.progr@gmail.com>
2021-01-25 16:19:17 +0100

    Zmiany nazewnictwa, logika pobierania i wyświetlania szczegółów postaci i pojazdów

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 export class App extends React.Component {
 
   state = {
-    searchResult: null
+    searchPeople: null,
+    searchVehicles: null
   };
 
   render() {
@@ -18,13 +19,16 @@ export class App extends React.Component {
               <AppHeader 
                 passedGetPeople={this.getPeople}
                 passedGetVehicles={this.getVehicles}
-                passedGetShips={this.getShips}
-                passedGetSearchResult={this.getSearchResult}
                 />
               <div className="container contacts">
-                {this.state.searchResult ? (
-                                  <ContactsList searchResult={this.state.searchResult} />
+                {this.state.searchPeople ? (
+                                  <ContactsList searchPeople={this.state.searchPeople} />
                                   ) : (
+                          <LoadingInfo />
+                          )}
+                {this.state.searchVehicles ? (
+                          <ContactsList searchVehicles={this.state.searchVehicles} />
+                          ) : (
                           <LoadingInfo />
                           )}
               </div>
@@ -35,39 +39,29 @@ export class App extends React.Component {
   // pobieranie osób
   getPeople = () => {
     fetch("https://swapi.dev/api/people/")
+            .then(this.setState({searchVehicles: null}))
             .then(res => res.json())
-            .then(json => this.setState({searchResult: json.results}));
+            .then(json => this.setState({searchPeople: json.results}));
   }
 
   // pobieranie pojazdów
   getVehicles = () => {
     fetch("https://swapi.dev/api/vehicles/")
+            .then(this.setState({searchPeople: null}))
             .then(res => res.json())
-            .then(json => this.setState({searchResult: json.results}));
-  }
-
-  // pobieranie statków
-  getShips = () => {
-    fetch("https://swapi.dev/api/starships/")
-            .then(res => res.json())
-            .then(json => this.setState({searchResult: json.results}));
-  }
-
-  // wyszukiwanie
-  getSearchResult() {
-    fetch("https://swapi.dev/api/starships/")
-            .then(res => res.json())
-            .then(json => this.setState({searchResult: json.results}));
+            .then(json => this.setState({searchVehicles: json.results}));
   }
 
 }
 
+// informacja, jeśli nic nie jest załadowane
 function LoadingInfo() {
   return(
-          <span className="loading-info">Wybierz opcję z górnego menu lub wyszukaj...</span>
+          <span className="loading-info"></span>
           )
 }
 
+// nagłówek
 class AppHeader extends React.Component {
   render() {
     return (
@@ -83,16 +77,14 @@ class AppHeader extends React.Component {
                     <div className="options">
             
                       <div className="single active"
-                           onClick={this.props.passedGetPeople}>
+                           onClick={this.props.passedGetPeople}
+                           >
                         Postacie
                       </div>
                       <div className="single active"
-                           onClick={this.props.passedGetVehicles}>
+                           onClick={this.props.passedGetVehicles}
+                           >
                         Pojazdy
-                      </div>
-                      <div className="single active"
-                           onClick={this.props.passedGetShips}>
-                        Statki
                       </div>
             
                     </div>
@@ -107,12 +99,6 @@ class AppHeader extends React.Component {
                       </form>
                     </div>
                     <div className="searchresults">
-                      <div className="one">Luke Skywalker</div>
-                      <div className="one">Luke Skywalker</div>
-                      <div className="one">Luke Skywalker</div>
-                      <div className="one">Luke Skywalker</div>
-                      <div className="one">Luke Skywalker</div>
-                      <div className="one">Luke Skywalker</div>
                     </div>
                   </div>
                 </div>
@@ -122,6 +108,7 @@ class AppHeader extends React.Component {
   }
 }
 
+// lista wyników
 class ContactsList extends React.Component {
 
   constructor(props) {
@@ -131,115 +118,183 @@ class ContactsList extends React.Component {
     };
   }
 
-  showPersonalInfo = (title, tel, email, city, postal, street, likes) => {
+  // pokaż szczegółowe informacje o wybranej postaci
+  showPersonDetailedInfo = (
+          title, gender, birth_year, eye_color, hair_color, homeworld, height, mass
+          ) => {
     this.setState(() => {
       return {
 // isDisplayed: !prevState.isDisplayed
         isDisplayed: true,
         title: title,
-        tel: tel,
-        email: email,
-        city: city,
-        postal: postal,
-        street: street,
-        likes: likes,
-        avatar: "url(img/avatar.webp)"
+        gender: gender,
+        birth_year: birth_year,
+        eye_color: eye_color,
+        hair_color: hair_color,
+        homeworld: homeworld,
+        height: height,
+        mass: mass
+      }
+    });
+  }
+
+  // pokaż szczegółowe informacje o wybranym pojeździe
+  showVehicleDetailedInfo = (
+          title, model, manufacturer, cost_in_credits, length, max_atmosphering_speed, crew, passengers
+          ) => {
+    this.setState(() => {
+      return {
+        isDisplayed: true,
+        title: title,
+        model: model,
+        manufacturer: manufacturer,
+        cost_in_credits: cost_in_credits,
+        length: length,
+        max_atmosphering_speed: max_atmosphering_speed,
+        crew: crew,
+        passengers: passengers
       }
     });
   }
 
   render() {
-    const {searchResult} = this.props;
-
-    return (
-            <div className="row">
-              {this.state.isDisplayed == true &&
-                          <PersonalInfo
-                            title={this.state.title}
-                            tel={this.state.tel}
-                            email={this.state.email}
-                            city={this.state.city}
-                            postal={this.state.postal}
-                            street={this.state.street}
-                            avatar={this.state.avatar}
-                            />
-              }
-              <PersonalProjects
-                isDisplayed={this.state.isDisplayed}
-                />
-            
-              {this.props.searchResult.map((oneResult, counter) => (
-                            <div className="col-md-3" key={counter}>
-                              <div className="single">
-                                <div className="main-info" 
-                                     onClick={() => this.showPersonalInfo(
-                                        oneResult.name,
-                                        oneResult.gender,
-                                        oneResult.birth_year,
-                                        oneResult.eye_color + ', ' + oneResult.hair_color,
-                                        oneResult.homeworld,
-                                        oneResult.height + ', ' + oneResult.mass, 
-                                        24
-                                        )}>
-                                  <div className="title">
-                                    {oneResult.name}
+    // zwracanie osób
+    if (this.props.searchPeople) {
+      return (
+              <div className="row">
+                {this.state.isDisplayed == true &&
+                              <PersonDetailedInfo
+                                title={this.state.title}
+                                gender={this.state.gender}
+                                birth_year={this.state.birth_year}
+                                eye_color={this.state.eye_color}
+                                hair_color={this.state.hair_color}
+                                homeworld={this.state.homeworld}
+                                height={this.state.height}
+                                mass={this.state.mass}
+                                />
+                }
+                <PersonPersonalProjects
+                  isDisplayed={this.state.isDisplayed}
+                  />
+              
+                {this.props.searchPeople.map((oneResult, counter) => (
+                                <div className="col-md-3" key={counter}>
+                                  <div className="single">
+                                    <div className="main-info" 
+                                         onClick={() => this.showPersonDetailedInfo(
+                                            oneResult.name,
+                                            oneResult.gender,
+                                            oneResult.birth_year,
+                                            oneResult.eye_color,
+                                            oneResult.hair_color,
+                                            oneResult.homeworld,
+                                            oneResult.height,
+                                            oneResult.mass
+                                            )}>
+                                      <div className="title">
+                                        {oneResult.name}
+                                      </div>
+                                    </div>
+                                    <div className="tel">
+                                      {oneResult.gender}
+                                    </div>
+                                    <div className="email">
+                                      {oneResult.birth_year}<br/>{oneResult.hair_color}<br/>{oneResult.eye_color}
+                                    </div>
                                   </div>
                                 </div>
-                                <div className="tel">
-                                  {oneResult.gender}
+                            ))}
+              </div>
+              )
+    }
+    // zwracanie pojazdów
+    if (this.props.searchVehicles) {
+      return (
+              <div className="row">
+                {this.state.isDisplayed == true &&
+                              <VehicleDetailedInfo
+                                title={this.state.title}
+                                model={this.state.model}
+                                manufacturer={this.state.manufacturer}
+                                cost_in_credits={this.state.cost_in_credits}
+                                length={this.state.length}
+                                max_atmosphering_speed={this.state.max_atmosphering_speed}
+                                crew={this.state.crew}
+                                passengers={this.state.passengers}
+                                />
+                }
+                <PersonPersonalProjects
+                  isDisplayed={this.state.isDisplayed}
+                  />
+              
+                {this.props.searchVehicles.map((oneResult, counter) => (
+                                <div className="col-md-3" key={counter}>
+                                  <div className="single">
+                                    <div className="main-info" 
+                                         onClick={() => this.showVehicleDetailedInfo(
+                                            oneResult.name,
+                                            oneResult.model,
+                                            oneResult.manufacturer,
+                                            oneResult.cost_in_credits,
+                                            oneResult.length,
+                                            oneResult.max_atmosphering_speed,
+                                            oneResult.crew,
+                                            oneResult.passengers
+                                            )}>
+                                      <div className="title">
+                                        {oneResult.name}
+                                      </div>
+                                    </div>
+                                    <div className="one-info">
+                                      {oneResult.model}
+                                    </div>
+                                    <div className="one-info">
+                                      {oneResult.manufacturer}
+                                    </div>
+                                    <div className="one-info">
+                                      {oneResult.cost_in_credits}
+                                    </div>
+                                  </div>
                                 </div>
-                                <div className="email">
-                                  {oneResult.birth_year}<br/>{oneResult.hair_color}<br/>{oneResult.eye_color}
-                                </div>
-                                <div className="counter" onClick={() => this.showPersonalInfo(
-                                        oneResult.name,
-                                        oneResult.gender,
-                                        oneResult.birth_year,
-                                        oneResult.eye_color + ', ' + oneResult.hair_color,
-                                        oneResult.homeworld,
-                                        oneResult.height + ', ' + oneResult.mass, 
-                                        24
-                                        )}>
-                                  <button className="add-like" onClick={this.increment.bind(this)}>
-                                    Zobacz
-                                  </button>
-                                </div>
-                              </div>
-                            </div>
-                          ))}
-            </div>
-            )
+                            ))}
+              </div>
+              )
+    }
   }
-  increment() {
-    console.log('todo increment');
-  }
-
 }
 
-class PersonalInfo extends React.Component {
+// info szczegółowe po kliknięciu w element
+class PersonDetailedInfo extends React.Component {
 
   render() {
-    const {title, tel, email, city, postal, street, isDisplayed} = this.props;
+    const {title, gender, birth_year, eye_color, hair_color, homeworld, height, mass, isDisplayed} = this.props;
     return (
             <div className="col-md-6 personal-info-container">
               <div className="personal-info">
                 <div className="title">
                   {title}
                 </div>
-                <div className="tel">
-                  {tel}
+                <div className="one-info">
+                  Płeć: {gender}
                 </div>
-                <div className="email">
-                  {email}
+                <div className="one-info">
+                  Urodzony: {birth_year}
                 </div>
-                <div className="city">
-                  {city}
+                <div className="one-info">
+                  Oczy: {eye_color}
                 </div>
-                <div className="postal">
-                  {postal}
+                <div className="one-info">
+                  Włosy: {hair_color}
                 </div>
-                <div className="street">
-                  {street}
+                <div className="one-info">
+                  Pochodzenie: {homeworld}
+                </div>
+                <div className="one-info">
+                  Wysokość: {height}
+                </div>
+                <div className="one-info">
+                  Masa: {mass}
                 </div>
               </div>
             </div>
@@ -247,7 +302,47 @@ class PersonalInfo extends React.Component {
   }
 }
 
-class PersonalProjects extends React.Component {
+
+// info szczegółowe po kliknięciu w element
+class VehicleDetailedInfo extends React.Component {
+
+  render() {
+    const {title, model, manufacturer, cost_in_credits, length, max_atmosphering_speed, crew, passengers, isDisplayed} = this.props;
+    return (
+            <div className="col-md-6 personal-info-container">
+              <div className="personal-info">
+                <div className="title">
+                  {title}
+                </div>
+                <div className="one-info">
+                  Model: {model}
+                </div>
+                <div className="one-info">
+                  Producent: {manufacturer}
+                </div>
+                <div className="one-info">
+                  Koszt: {cost_in_credits} kredytów
+                </div>
+                <div className="one-info">
+                  Długość: {length} m.
+                </div>
+                <div className="one-info">
+                  Max. prędkość atmosferyczna: {max_atmosphering_speed} km/h
+                </div>
+                <div className="one-info">
+                  Ilość załogi: {crew}
+                </div>
+                <div className="one-info">
+                  Ilość pasażerów: {passengers}
+                </div>
+              </div>
+            </div>
+            )
+  }
+}
+
+// szczegółowe info prawa kolumna
+class PersonPersonalProjects extends React.Component {
 
   render() {
     const {isDisplayed} = this.props;
@@ -267,6 +362,7 @@ class PersonalProjects extends React.Component {
   }
 }
 
+// prawa kolumna jedna kategoria
 class ProjectItem extends React.Component {
 
   render() {
